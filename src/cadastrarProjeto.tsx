9f809b654b06e25482b042e6279a31c385542b04
Henrique Matos <henriquemundimmatos@gmail.com>
2024-01-28 20:18:49 -0300

    feat: add style

--- src/cadastrarProjeto.tsx
+++ src/cadastrarProjeto.tsx
@@ -1,6 +1,9 @@
 import { useForm } from "react-hook-form"
 import { FormProject, schemaRegisterProject } from "./ZodSchemas/schema-register-project"
+import gitHub from './assets/GithubLogo.svg'
+import monitor from './assets/Monitor.svg'
 import styles from './cadastrarProjeto.module.css'
+
 
 export function CadastrarProduto() {
 
@@ -15,16 +18,22 @@ export function CadastrarProduto() {
   return (
     <main>
       <div className={styles.container}>
-        <h1>Cadastre seu projeto</h1>
+        <h1>CADASTRE SEU PROJETO</h1>
         <form onSubmit={handleSubmit(handleFormSubmit)} className={styles['register_form']}>
-          <input type="text" id="title" placeholder="Titulo" {...register('title')}/>
+          <div className={styles['title-container']}>
+            <input type="text" id="title" placeholder="Titulo" {...register('title')}/>
+            <div className={styles['icon-container']}>
+              <img src={monitor} alt="" />
+              <img src={gitHub} alt="" />
+            </div>
+          </div>
           <textarea  id="description" placeholder="Mensagem" {...register('description')}></textarea>
 
           <div className={styles['container_file']}>
-            <button></button>
+            <button className={styles.archive}></button>
             <p>Adicione uma imagem</p>
           </div>
-          <button type="submit">Enviar</button>
+          <button type="submit">ENVIAR</button>
         </form>
       </div>
     </main>
